294fe9cce6e0c8b799ab2fcfef32af92a7ea229f
Priyanshu Belwal <priyanshubelwal463@gmail.com>
2024-11-06 17:14:10 +0530

    Replace some lines by my lines

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true
 });
 const db = mongoose.connection;
@@ -14,6 +12,8 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -25,6 +25,11 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 });
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
