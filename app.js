c3a2beb87238315fd48155fc807cd063885b61b0
Priyanshu Belwal <priyanshubelwal463@gmail.com>
2024-11-07 16:03:14 +0530

    add delete button

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-
-
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -21,10 +20,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
 
-const methodOverride = require('method-override');
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -60,6 +59,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
 })
 
 app.listen(3000, () => {
