159d459cab05cae07f3ce475bbd6b6dfaf1c5c1c
Priyanshu Belwal <priyanshubelwal463@gmail.com>
2024-11-09 16:22:37 +0530

    add Boilerplate and navbar files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsmate = require('ejs-mate')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -16,7 +17,7 @@ db.once("open", () => {
 const app = express();
 
 
-
+app.engine('ejs', ejsmate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
