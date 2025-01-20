aff18ae173b9581a156a6c78870428e36671d57c
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-20 23:51:35 +0530

    Improved functionality after fb5b357

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ const mongoSanitize = require('express-mongo-sanitize')
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const dbUrl = process.env.DB_URL;
+mongoose.connect(dbUrl);
+// mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
